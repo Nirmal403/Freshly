d50a75366f5f2fdb709a4b04672384dc778cfee7
Nirmal403 <nirmalcool414@gmail.com>
2022-05-04 12:07:10 +0530

    new update

---
+++ footer.js
@@ -0,0 +1,53 @@
+const footer=()=>{
+
+    
+
+return `<div id="footermain">
+
+<div id="footer">
+  <div id="foot1">
+    <div id="footimg">
+      <a><img src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5fc4feea074f5b86a6c62285_freshly-logo-R.svg"></a>
+    </div>
+    <div id="inc">© Freshly Inc.</div>
+  </div>
+
+  
+  <div id="foot2">
+    <div id="foothead">Company</div>
+    <div><a href="">Careers</a></div>
+    <div><a href="">Contact Us</a></div>
+    <div><a href="">Press</a></div>
+    <div><a href="">Terms</a></div>
+    <div><a href="">Privacy</a></div>
+    <div><a href="">Affiliates</a></div>
+    <div><a href="">Partnerships</a></div>
+  </div>
+  <div id="foot3">
+    <div id="foothead">Learn More</div>
+    <div><a href="">Plans & Menu</a></div>
+    <div><a href="">Why Freshly?</a></div>
+    <div><a href="">Gifts</a></div>
+    <div><a href="">FAQs</a></div>
+    <div><a href="">Blog</a></div>
+    <div><a href="">Students</a></div>
+  </div>
+  
+  <div id="foot4">
+    <div id="foothead">Join Us</div>
+    <div id="media">
+      <a href="#"><img src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5f52423bbc67c09879c5a19b_5d795c37fedbb6017d824b02_icons8-facebook-old-30.jpg" alt=""></a>
+      <a href="#"><img src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5f52423ad92101de48f410c7_5d10dfc0ef515f74b875a42c_icons8-instagram-50.jpg" alt=""></a>
+      <a href="#"><img src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5f52423bd050163202b341e8_5d10dfc00442bfc99d3d6828_icons8-twitter-filled-50.jpg" alt=""></a>
+      <a href="#"><img src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5f52423a6b26e039d505425e_5d10dfbfef515faad975a42b_icons8-pinterest-filled-50.jpg" alt=""></a>
+    </div>
+    <div class="app"><a  href="#"><img src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5d6439d4fb92e7630e46976b_Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.svg" alt=""></a></div>
+    <div class="app"><a  href="#"><img src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5d643dd558306bfe1df5ef8b_google-play-badge.png" alt=""></a></div>
+  </div>
+</div>
+
+
+</div>`;
+}
+
+export default footer;
---
+++ navbar.js
@@ -0,0 +1,42 @@
+const navbar=()=>{
+    return `
+    <div id="navbar">
+        <div class="navlogo"> 
+            <img src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5fc4feea074f5b86a6c62285_freshly-logo-R.svg" alt="" srcset=""">
+        </div>
+        <div>
+            <img src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5f7b2e5362c26e009ed80a5a_bizlogo_gray-09.svg">
+        </div>
+    </div>
+
+    <div id="navbar1">
+            <div id="logo">
+                <a href="index.html"><img id="logopic" src="https://assets-global.website-files.com/5d03b4e130118314af624b20/5fc4feea074f5b86a6c62285_freshly-logo-R.svg" alt=""></a>
+            </div>
+            <div id="pages">
+                <div>
+                    <a href="">Plans & Menu</a>
+                </div>
+                <div>
+                    <a href="">How it Works</a>
+                </div>
+                <div>
+                    <a href="">Gifts</a>
+                </div>
+                <div>
+                    <a href="">Refer a Friend</a>
+                </div>
+                <div>
+                    <a href="">Help</a>
+                </div>
+            </div>
+            <div>
+                <div id="login">
+                    <div id="log"><a href="">Login</a></div>
+                    <button id="sign"><a href="">Sign Up</a></button>
+                </div>
+            </div>
+    </div>`;
+}
+
+export default navbar;
